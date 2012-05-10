50316554912cbccde14ef90b240ee12c26fe23a5
vol4ok <github@vol4ok.net>
2012-05-10 11:02:50 +0300

    update version and copyright in _header.js

--- src/_header.js
+++ src/_header.js
@@ -1,6 +1,10 @@
+/*!
+ * Copyright (c) 2012 Andrew Volkov <hello@vol4ok.net>
+ */
+ 
 var Core = (function() {
     Core.constructor = function(){};
-    Core.VERSION = "0.1.2";
+    Core.VERSION = "0.2.0";
     function Core() { return Core.constructor.apply(this, arguments) }
     return Core;
 })();
